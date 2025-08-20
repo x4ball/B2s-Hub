d58e9be02e223f695a5f77ebb3a5f83ab3655d86
x4ball <fayrixt@gmail.com>
2025-08-21 02:11:48 +0700

    Update cdidv1.lua

--- cdidv1.lua
+++ cdidv1.lua
@@ -1,9 +1,9 @@
 local WindUI = loadstring(game:HttpGet("https://github.com/Footagesus/WindUI/releases/latest/download/main.lua"))()
-local layar = WindUI:CreateWindow({
-    Title = "B2S HUB | Car Driving Indonesian",
-    Icon = "shield-check",
-    Author = "BY -mhdfa1z",
-    Folder = "B2S HUB | CDID",
+local Window = WindUI:CreateWindow({
+    Title = "B2s Hub | Car Driving Indonesian",
+    Icon = "earth",
+    Author = "Made By @O28Fx",
+    Folder = "Dealership",
     
     Size = UDim2.fromOffset(580, 460),
     Transparent = true,
@@ -11,37 +11,14 @@ local layar = WindUI:CreateWindow({
     Resizable = true,
     SideBarWidth = 200,
     BackgroundImageTransparency = 0.42,
-    HideSearchBar = true,
+    HideSearchBar = false,
     ScrollBarEnabled = false,
 })
-local sektionmain = layar:Section({
-    Title = "Main",
-    Icon = "folder",
-})
-local maintab = sektionmain:Tab({
-    Title = "Welcome",
-    Icon = "smile",
-})
-local sektionmaintab = maintab:Section({ 
-    Title = "Home",
-    Icon = "",
-})
-local paragafmaintab = sektionmaintab:Paragraph({
-    local player = game.Players.LocalPlayer
-    Title = "Welcome to B2S HUB Script " .. player.Name,
-    Desc = "For you all if this script have a bug or somthing make you lag repots the bug in B2S HUB discord.",
-    Thumbnail = "https://i.ibb.co.com/67m3JC4m/static.png",
-    ThumbnailSize = 460,
-    Locked = false,
-    Buttons = {
-        {
-            Icon = "copy",
-            Title = "Join B2s Hub Discord",
-            Callback = function() 
-             setclipboard("https://discord.gg/xxxxxx")
-             WindUI:Notify({
-              Title = "Discord link already copied",
-                   Duration = 2,
-                     Icon = "bell",})
-        }
-})
+Window:Tag({Title = "Work",Color = Color3.fromHex("#30ff6a")})
+Window:Tag({Title = "Beta Script",Color = Color3.fromHex("#30ff6a")})
+WindUI:Notify({title = "Welcome Free User!",Content = "Enjoy We Are Script :)",Duration = 2,Icon = "bell",})
+local MainSection = Window:Section({Title = "Main",Opened = false,})
+local MainTab = MainSection:Tab({Title = "Welcome",Icon = "house",Locked = false,})
+MainTab:Paragraph({Title = "Welcome To B2s Hub Script!!",Desc = "I Hope You Enjoy B2s Hub,If the script have bug or somthing wrong, you can repots the bug in B2s Hub Server :]",Thumbnail = "https://i.ibb.co/1GVxksFK/static.png",ThumbnailSize = 460,Locked = false,Buttons = {{Icon = "square-arrow-out-up-right",Title = "Join The B2s Hub Discord",Callback = function() setclipboard("https://discord.gg/b2shub") end,}}})
+local ChangelogTab = MainSection:Tab({Title = "Changelogs",Icon ="square-pen",Locked = false,})
+ChangelogTab:Paragraph({Title = "Changelog For Cdid V1.1",Desc = "-Added new tab for teleport to a dealership and job"})
